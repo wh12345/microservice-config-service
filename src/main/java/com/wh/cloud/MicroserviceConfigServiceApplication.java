e3976765eb11c88305e916aff994cb966f4ba6f0
wh12345 <1439894056@qq.com>
2019-06-17 11:17:28 +0800

    提交一下

---
+++ src/main/java/com/wh/cloud/MicroserviceConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigServiceApplication.class, args);
+	}
+
+}
